1abaa87ada48bf45bfcb0728eae1d5b834fb2959
Matthew Carnevale <mcarnevale@ecash.com.au>
2018-02-06 10:07:39 +1100

    Working on scoring cases for strikes and spares. Simplified the data model again to make counting throws easier.

--- scorecard.fs
+++ scorecard.fs
@@ -19,10 +19,18 @@ type FrameScore =
     | BuildingScore of int
     | Scored of int
 
-type Frame = {progress:FrameProgression;throws:Ball list; score:FrameScore}
+type Frame =
+    { progress : FrameProgression
+      throws : int list
+      score : FrameScore }
+
 type Game = Frame list
 
-let newFrame = {progress=Unplayed;throws=[];score=Unscored}
+let newFrame =
+    { progress = Unplayed
+      throws = []
+      score = Unscored }
+
 let createNewGame = [ newFrame ]
 
 
@@ -31,64 +39,79 @@ let scoreCard = createNewGame
 let getCurrentFrame game = List.head game
 
 
-let classifyBall pinsKnockedDown =
-    match pinsKnockedDown with
-    | 0 -> Gutter
-    | i when i <= 9 -> Hit i
-    | numberOfPinsInBowling -> Ball.Strike
-    | _ -> Invalid
+////////////////////////////////////////////////////////////////////////////
+// Utility functions for frames
+////////////////////////////////////////////////////////////////////////////
 
+let frameIsStrike frame =
+    match frame.throws with
+    | [x; _] when x=numberOfPinsInBowling -> true
+    | _ -> false
 
-let ballScore ball =
-    match ball with
-    | Gutter -> 0
-    | Hit i -> i
-    | Ball.Strike -> numberOfPinsInBowling
-    | Invalid -> 0
-
+let frameIsSpare frame =
+    match frame.throws with
+    | [x;y] when x+y=numberOfPinsInBowling -> true
+    | _ -> false
+    
     
 // Given a frame and a number of pins knocked down, return the new frame
 let updateFrame frame pinsKnockedDown =
-    let
-        ball = classifyBall pinsKnockedDown
-    in
-        match frame.progress with
-            | Unplayed ->
-                if pinsKnockedDown <> numberOfPinsInBowling then
-                    {progress=InProgress; throws=[ball];score=BuildingScore pinsKnockedDown}
-                else
-                    {progress=Complete; throws=ball::frame.throws;score=BuildingScore 10}
-            | InProgress -> 
-                let totalPins = (frame.throws |> List.sumBy ballScore) + pinsKnockedDown
-                let finalScore = if totalPins <> numberOfPinsInBowling then Scored totalPins else BuildingScore numberOfPinsInBowling
-                let progress = if totalPins=10 or (List.length frame.throws)=1 then Complete else InProgress
-                in {progress=Complete; throws=ball::frame.throws;score=finalScore}
-            | Complete ->
-                frame
+    match frame.progress with
+        | Unplayed ->
+            if pinsKnockedDown <> numberOfPinsInBowling then
+                { progress = InProgress; throws = [pinsKnockedDown]; score = BuildingScore pinsKnockedDown}
+            else
+                { progress = Complete; throws = pinsKnockedDown::frame.throws; score = BuildingScore numberOfPinsInBowling}
+        | InProgress -> 
+            let totalPins = (frame.throws |> List.sum) + pinsKnockedDown
+            let finalScore = if totalPins <> numberOfPinsInBowling then Scored totalPins else BuildingScore numberOfPinsInBowling
+            let progress = if totalPins=numberOfPinsInBowling or (List.length frame.throws)=1 then Complete else InProgress
+            in {progress=Complete; throws=pinsKnockedDown::frame.throws;score=finalScore}
+        | Complete ->
+            frame
 
 // Recalculate the scores for a game
+let getNumericScoreValue score =
+    match score with
+    | Unscored -> 0
+    | BuildingScore s -> s
+    | Scored s -> s
+
 let updateScores game =
     let scoreBeingBuilt sc =
         match sc with
         | BuildingScore _ -> true
         | _ -> false
     in
-        game
+        match game with
+        // Strike scoring cases
+        | fr1::fr::rest when (scoreBeingBuilt fr.score) && (frameIsStrike fr) && (not <| frameIsStrike fr1) && (fr1.progress=Complete) ->
+            fr1::{fr with score=Scored
+                       (10 + (getNumericScoreValue fr1.score))}::rest
+        | fr2::fr1::fr::rest when (scoreBeingBuilt fr.score) && (frameIsStrike fr) && (frameIsStrike fr1) && (fr2.progress=Complete) ->
+            fr2::fr1::{fr with score=Scored
+                            (20 + (getNumericScoreValue fr2.score))}::rest
+        // Spare scoring case
+        | fr1::fr::rest when (scoreBeingBuilt fr.score) && (frameIsSpare fr) && not <| (frameIsStrike fr1) ->
+            fr1::{fr with score=Scored
+                        (10 + (List.head fr1.throws))}::rest
+        | _ -> game
+        
 
 let framesInGame game =
     List.length game
 
     
 // Our main API function
-let submitBowl game pinsKnockedDown =
+let submitBowl pinsKnockedDown game =
     let currentFrame = getCurrentFrame game
     let updatedFrame = updateFrame currentFrame pinsKnockedDown
     let gameFinished = (List.length game)=numberOfFramesInBowling
         
     let newGame =
         if not gameFinished then
-            if updatedFrame.progress <> Complete then updatedFrame :: List.tail game else newFrame :: updatedFrame :: List.tail game
+            if updatedFrame.progress <> Complete then updateScores <| updatedFrame :: List.tail game else newFrame :: (updateScores <| updatedFrame :: List.tail game)
         else
             game
     in
-      updateScores newGame
+      newGame
