818e347be4e87c26ee5ea67a133c5d96e3eada79
Matthew Carnevale <mcarnevale@ecash.com.au>
2018-02-12 08:57:26 +1100

    Tidy up some layout and consolidate strike/spare evaluation.

--- scorecard2.fs
+++ scorecard2.fs
@@ -18,19 +18,29 @@ type FrameF =
 type Frame = FrameR of FrameR | FrameF of FrameF
 type game = Frame list
 
+
 let createNewGame = [Unplayed]
+
+
+let isStrike pins = pins=10
+let isSpare p1 p2 = (p1+p2)=10
 
 let evaluateBowl pins game =
     match game with
     // Cases for the non-final frames
-    | FrameR Unplayed::rest -> if pins=10 then FrameR Strike::rest else FrameR (InProgress pins)::rest
-    | FrameR (InProgress p1)::rest -> if pins+p1=10 then FrameR (Spare (p1,pins))::rest else FrameR (Complete (p1,pins))::rest
+    | FrameR Unplayed::rest -> if isStrike pins then FrameR Strike::rest
+                                                else FrameR (InProgress pins)::rest
+    | FrameR (InProgress p1)::rest -> if isSpare p1 pins then FrameR (Spare (p1,pins))::rest
+                                                         else FrameR (Complete (p1,pins))::rest
     // Cases for the final frame
-    | FrameF UnplayedF::rest -> if pins=10 then FrameF (InProgressBonusF1 pins)::rest else FrameF (InProgressF pins)::rest
-    | FrameF (InProgressF p1)::rest -> if pins+p1=10 then FrameF (InProgressBonusF2 (p1,pins))::rest else FrameF (CompleteF (p1,pins))::rest
+    | FrameF UnplayedF::rest -> if isStrike pins then FrameF (InProgressBonusF1 pins)::rest
+                                                 else FrameF (InProgressF pins)::rest
+    | FrameF (InProgressF p1)::rest -> if isSpare p1 pins then FrameF (InProgressBonusF2 (p1,pins))::rest
+                                                          else FrameF (CompleteF (p1,pins))::rest
     | FrameF (InProgressBonusF1 p1)::rest -> FrameF (InProgressBonusF2 (p1,pins))::rest
     | FrameF (InProgressBonusF2 (p1,p2))::rest -> FrameF (CompleteBonusF (p1,p2,pins))::rest
     | _ -> game // Really should error here?
+
 
 let submitBowl pins game =
     let topFrameFinished game =
@@ -46,7 +56,9 @@ let submitBowl pins game =
         | FrameF (CompleteF (_,_))::_ -> true
         | FrameF (CompleteBonusF (_,_,_))::_ -> true
         | _ -> false
+    
     let newGame = evaluateBowl pins game
+
     if topFrameFinished newGame then
         if gameFinished newGame then newGame
         else if (List.length game) < 9 then FrameR Unplayed::newGame else FrameF UnplayedF::newGame
